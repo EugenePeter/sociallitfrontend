4eb491cdd34e8b23be1c102b55c5fef53dc8fe02
eugene <eugenepdm@gmail.com>
2020-06-03 13:11:40 +0800

    deleted unkown files

--- app/components/FlashMessages.js
+++ app/components/FlashMessages.js
@@ -1,43 +1,5 @@
 import React, { useEffect } from "react";
 
-import DispatchContext from "../../context/DispatchContext";
-import StateContext from "../../context/StateContext";
-
-import { useImmerReducer, useImmer } from "use-immer";
-
-import {
-  Container,
-  ContainerNarrower,
-  TitleContainer,
-  ContainerRow,
-} from "../../global-styles/global.styles";
-
-import { GlobalButton, BackBtn } from "../../global-styles/GlobalButton.styles";
-
-import {
-  FormContainer,
-  FormInputContainer,
-  ButtonWrapper,
-  FormInputSpan,
-  GroupContainer,
-} from "../../components/Form/form-input/form-input.styles";
-
-import {
-  RadioInput,
-  IconsWrapper,
-} from "../../components/Form/radio-button/radio-button-styles";
-
-// import RadioBlockComponent from "../../components/Form/";
-import { JobType } from "../../components/Form/job-type/radioBtnContainer";
-// import { RadioContainer } from "../../components/Form/tabs-component/RadioInputBlock.styles";
-
-import RadioIcons from "../../components/Form/radio-button/radio-button.component";
-import {
-  RadioContainer,
-  IconContainer,
-  IconRow,
-} from "../../components/Form/radio-button/radio-button-styles";
-
 function FlashMessages(props) {
   return (
     <div className="floating-alerts">
